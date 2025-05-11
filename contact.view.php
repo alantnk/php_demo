4b4a932119ca3096d9252e382fd4da9115a3e7db
alan <alan@tnk.com>
2025-05-11 17:59:35 -0300

    feat: page links

--- contact.view.php
+++ contact.view.php
@@ -21,10 +21,10 @@
                         <div class="block">
                             <div class="ml-10 flex items-baseline space-x-4">
 
-                                <a href="/" class="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white" aria-current="page">Home</a>
+                                <a href="/" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white" aria-current="page">Home</a>
                                 <a href="/about.php" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">About</a>
                                 <a href="#" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Notes</a>
-                                <a href="/contact.php" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Contact</a>
+                                <a href="/contact.php" class="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white">Contact</a>
 
                             </div>
                         </div>
@@ -67,12 +67,12 @@
 
         <header class="bg-white shadow-sm">
             <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-                <h1 class="text-3xl font-bold tracking-tight text-gray-900">Home</h1>
+                <h1 class="text-3xl font-bold tracking-tight text-gray-900">Contact</h1>
             </div>
         </header>
         <main>
             <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-                Home Page
+                Contact Page
             </div>
         </main>
     </div>
