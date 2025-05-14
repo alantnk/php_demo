08edbc77336c398117451df607bc274b379b7fd3
alan <alan@tnk.com>
2025-05-14 16:54:13 -0300

    feat: form validation

--- controllers/note-create.php
+++ controllers/note-create.php
@@ -6,9 +6,20 @@ $db = new Database($config['database']);
 
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $db->query("INSERT INTO notes.notes (body, user_id) VALUES(:body, :user_id)", [
-        "body" => $_POST['body'],
-        "user_id" => 2
-    ]);
+    $errors = [];
+    if (strlen($_POST['body']) === 0) {
+        $errors['body'] = 'A body is required';
+    }
+
+    if (strlen($_POST['body']) > 500) {
+        $errors['body'] = 'The body can not be more than 500 characters';
+    }
+
+    if (empty($errors)) {
+        $db->query("INSERT INTO notes.notes (body, user_id) VALUES(:body, :user_id)", [
+            "body" => $_POST['body'],
+            "user_id" => 2
+        ]);
+    }
 }
 require 'views/note-create.view.php';
--- views/note-create.view.php
+++ views/note-create.view.php
@@ -7,8 +7,14 @@
     <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
         <form method="POST">
             <label for="body">Text:</label>
-            <div>
-                <textarea id="body" name="body" class="bg-blue-100 p-1 w-96 h-48" placeholder="You text here"></textarea>
+            <div class="mb-2">
+                <textarea id="body" name="body" class="bg-blue-100 p-1 w-96 h-48" placeholder="You text here"><?= $_POST['body'] ?? "" ?></textarea>
+                <?php if (isset($errors['body'])) : ?>
+                    <p class="text-red-500 text-sm mb">
+                        <?= $errors['body']; ?>
+                    </p>
+                <?php endif; ?>
+
             </div>
             <p>
                 <button type="submit" class="cursor-pointer px-1 rounded hover:border">Create</button>
