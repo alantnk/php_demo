db1e043cd1e738711e1252ccf7cb37311852d1a2
alan <alan@tnk.com>
2025-05-21 12:27:34 -0300

    feat: middleware

--- Core/Router.php
+++ Core/Router.php
@@ -4,6 +4,8 @@
 
 namespace Core;
 
+use Core\Middleware\Middleware;
+
 class Router
 {
     protected $routes = [];
@@ -13,37 +15,40 @@ class Router
         $this->routes[] = [
             'uri' => $uri,
             'controller' => $controller,
-            'method' => $method
+            'method' => $method,
+            'middleware' => null
         ];
+
+        return $this;
     }
 
     public function get($uri, $controller)
     {
-        $this->add('GET', $uri, $controller);
+        return $this->add('GET', $uri, $controller);
     }
 
 
     public function post($uri, $controller)
     {
-        $this->add('POST', $uri, $controller);
+        return $this->add('POST', $uri, $controller);
     }
 
 
     public function delete($uri, $controller)
     {
-        $this->add('DELETE', $uri, $controller);
+        return $this->add('DELETE', $uri, $controller);
     }
 
 
     public function patch($uri, $controller)
     {
-        $this->add('PATCH', $uri, $controller);
+        return $this->add('PATCH', $uri, $controller);
     }
 
 
     public function put($uri, $controller)
     {
-        $this->add('PATCH', $uri, $controller);
+        return $this->add('PATCH', $uri, $controller);
     }
 
     public function route($uri, $method)
@@ -51,10 +56,18 @@ class Router
 
         foreach ($this->routes as $route) {
             if ($route['uri'] === $uri && $route['method'] === strtoupper($method)) {
+                Middleware::resolve($route['middleware']);
                 return require base_path($route['controller']);
             }
         }
         $this->abort();
+    }
+
+    public function only($key)
+    {
+        $last_key = array_key_last($this->routes);
+        $this->routes[$last_key]['middleware'] = $key;
+        return $this;
     }
 
     public function abort($code = 404)
