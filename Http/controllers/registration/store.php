3d7960db53b21f3d278bd04dc525bc9d2de68701
alan <alan@tnk.com>
2025-05-23 18:26:00 -0300

    fix: Authenticator methods

--- Http/controllers/registration/store.php
+++ Http/controllers/registration/store.php
@@ -1,21 +1,22 @@
 <?php
 
 use Core\App;
+use Core\Authenticator;
 use Core\Database;
 use Core\Validator;
 
 $db = App::resolve(Database::class);
+
 $email = $_POST['email'];
 $password = $_POST['password'];
 
-// Validate the form inputs
 $errors = [];
-if (! Validator::email($email)) {
+if (!Validator::email($email)) {
     $errors['email'] = 'Please provide a valid email address.';
 }
 
-if (! Validator::string($password, 6, 12)) {
-    $errors['password'] = 'Please provide a 6-12 length password.';
+if (!Validator::string($password, 7, 255)) {
+    $errors['password'] = 'Please provide a password of at least seven characters.';
 }
 
 if (! empty($errors)) {
@@ -24,23 +25,21 @@ if (! empty($errors)) {
     ]);
 }
 
+$user = $db->query('select * from users where email = :email', [
+    'email' => $email
+])->find();
 
-
-// Check if the resource already exists
-
-$user = $db->query('select * from users where email = :email', ['email' => $email])->find();
 if ($user) {
-    // If yes, redirect to login page
     header('location: /');
-    die();
+    exit();
 } else {
-    // If not, save one to the database, then log user in, and the redirect
-    $db->query('INSERT INTO notes.users (email, password) VALUES(:email, :password)', [
+    $user = $db->query('INSERT INTO users(email, password) VALUES(:email, :password)', [
         'email' => $email,
         'password' => password_hash($password, PASSWORD_BCRYPT)
     ]);
 
-    login($user);
+    (new Authenticator)->login(['email' => $email]);
+
     header('location: /');
-    die();
+    exit();
 }
